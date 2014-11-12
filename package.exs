f1887a978537093dcde3f5390e82afdba611a39f
Kenji Rikitake <kenji.rikitake@acm.org>
2014-11-12 23:54:50 +0900

    Add package.exs skeleton

---
+++ package.exs
@@ -0,0 +1,39 @@
+defmodule Tinymt.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :tinymt,
+     version: "0.2.0",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  def application do
+    [applications: [:logger]]
+  end
+
+  defp deps do
+    []
+  end
+
+  defp description do
+    """
+    Tiny Mersenne Twister (TinyMT) for Erlang
+    """
+  end
+
+  defp package do
+    [files: [
+        "package.exs"
+        ],
+     contributors: [
+        "Kenji Rikitake",
+        "Mutsuo Saito",
+        "Makoto Matsumoto"
+        ],
+     licenses: ["simplified BSD"],
+     links: %{"GitHub" => "https://github.com/jj1bdx/tinymt-erlang/"}
+     ]
+  end
+end
